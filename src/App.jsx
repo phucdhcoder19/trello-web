d8b1295f7d5aa4b86a073427b99ecaa0215d166a
phucdhcoder19 <dhphuc1406@gmail.com>
2025-01-26 13:21:02 +0700

    config mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography from '@mui/material/Typography'
 function App() {
   return (
     <>
-    <div>hoangphucdev</div>
+      <div>hoangphucdev</div>
+      <Typography variant="body2" color="text.secondary">Test Typo</Typography>
+      <Typography>body1</Typography>
+      <Button>Button</Button>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
-      <AccessAlarmIcon/>
+      <AccessAlarmIcon />
       <ThreeDRotation />
     </>
   )
