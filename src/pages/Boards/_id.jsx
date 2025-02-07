d49462ec36d3d1b24cf085b6653ef6d8ecef54a6
phucdhcoder19 <dhphuc1406@gmail.com>
2025-02-07 11:31:04 +0700

    relative absolute import

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,8 +1,8 @@
 //Boards detail
 import Container from '@mui/material/Container'
-import AppBar from '../../components/AppBar/index'
-import BoardBar from './BoardBar/index'
-import BoardContent from './BoardContent/index'
+import AppBar from '~/components/AppBar/index'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
 function Board() {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
